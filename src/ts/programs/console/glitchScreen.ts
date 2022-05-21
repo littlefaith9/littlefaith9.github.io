a75208a7a19745096f738225afa615a5516b70c7
StephanySonn <74612055+littlefaith9@users.noreply.github.com>
2022-05-22 01:22:04 +0800

    new glitch

--- src/ts/programs/console/glitchScreen.ts
+++ src/ts/programs/console/glitchScreen.ts
@@ -30,22 +30,39 @@ export class GlitchScreen {
     }
     private async clear() {
         this.console.clear();
+        this.console.currentPos.x = 0;
+        this.console.currentPos.y = this.console.h - 5;
+        this.console.print(times(this.console.w, () => 250));
+        this.console.currentPos.x = Math.floor(this.console.w / 2) - 8;
         this.console.print('starting GUI...');
+        this.console.currentPos.x = 0;
+        this.console.currentPos.y = this.console.h - 5;
+        await new Promise(res => setTimeout(res, 1000))
+        for (let i = 0; i < 32 + Math.random() * 16; i++) {
+            this.console.print('>');
+            await new Promise(res => setTimeout(res, 1));
+        }
         await new Promise(res => setTimeout(res, 1000))
     }
     private glitch() {
+        this.console.posIndex = this.console.w * this.console.h - 1;
+        this.console.print('Unsupported platform');
         return new Promise<void>(res => {
+            let count = 0;
             const intv = setInterval(() => {
-                if (!this.running) {
+                if (!this.running || count++ > 64 && Math.random() < 0.1) {
                     clearInterval(intv);
                     res();
                 }
+                if (count % 10 === 1) Beeper.beep(5);
+                while (Math.random() < 0.2) {
+                    const length = Math.floor(Math.random() * this.console.w);
+                    this.console.print(times(length, () => 32));
+                }
                 const length = Math.floor(Math.random() * this.console.w);
                 const randomString = times(length, () => Math.floor(Math.random() * 256));
-                Beeper.beep(1);
-                this.console.posIndex = this.console.w * this.console.h - 1;
                 this.console.print(randomString);
-            }, 500);
+            }, 200);
         });
     }
 }
